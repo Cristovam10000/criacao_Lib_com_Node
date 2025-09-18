1c71d91443ff60125b0ecfe4649d54d87426982b
CrisDev <cristovam1712@gmail.com>
2025-09-18 16:50:53 -0300

    criação da função assincrona criarEsalvarArquivos criação do arquivo helpers filtra ocorrencias e monta a logica para mostrar as palavras que se repetem

--- src/cli.js
+++ src/cli.js
@@ -1,26 +1,50 @@
 import fs from 'fs';
 import tratarErros from './erros/funcoesErros.js';
 import { contaPalavras } from './index.js';
+import { json } from 'stream/consumers';
+import { montaSaidaArquivo } from './helpers.js';
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
+const endereco = caminhoArquivo[3];
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
     try {
         if (erro) throw erro
-        contaPalavras(texto);
+        const resultado = contaPalavras(texto);
+        criaESalvarArquivos(resultado, endereco)
     } catch (erro) {
         tratarErros(erro);
     }
 })
 
-function criarESalvarArquivos(listaPalavras, endereco) {
+  async function criaESalvarArquivos(listaPalavras, endereco) {
     const arquivoNovo = `${endereco}/resultado.txt`;
-    const textoPalavras =   JSON.stringify(listaPalavras);
+    const textoPalavras =   montaSaidaArquivo(listaPalavras)
+
+
     try {
-      
+      await fs.promises.writeFile(arquivoNovo, textoPalavras);
+      console.log('arquivo crido');
     }catch (erro) {
 
     }
 }
 
+
+// function criaESalvarArquivos(ListarPalavras, endereco) {
+
+//     const arquivoNovo = `${endereco}/resultado.txt`
+//     const textoPalavras  = JSON.stringify(ListarPalavras)
+
+//     fs.promises.writeFile(arquivoNovo, textoPalavras)
+//     .then(() => {
+//         console.log('arquivo criado');
+//     })
+//     .catch((erro) => {
+//         throw erro
+//     })
+//     .finally(() => {
+//         console.log('operação finalizada')
+//     })
+// }
